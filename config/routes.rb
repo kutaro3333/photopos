0f3670c5a310f3f405f06b123184df925f722e3c
kutaro3333 <chinami0370@gmail.com>
2020-05-08 16:15:55 +0900

    投稿機能実装まで

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
     controllers: { registrations: 'registrations' }
 
   root 'pages#home'
+
   get '/users/:id', to: 'users#show', as: 'user'
+
+  get '/posts/new', to: 'posts#new'
+  post '/posts', to: 'posts#create'
+  post '/posts/:post_id/photos', to: 'photos#create', as: 'post_photos'
 end
